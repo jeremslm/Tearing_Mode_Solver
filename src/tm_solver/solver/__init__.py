782142e77804f4bd51620aac4abf2583de177f53
Jérémy Salm <130314261+jeremslm@users.noreply.github.com>
2024-05-28 13:43:38 +0200

    Update __init__.py

--- src/tm_solver/solver/__init__.py
+++ src/tm_solver/solver/__init__.py
@@ -3,5 +3,5 @@ FILE NAME : delta_prime.py
 WRITTEN BY : Salm Jérémy, EPFL (jeremy.salm@epfl.ch)
 CREATED IN : April 2024
 """
-from .delta_prime import *
-__all__ = delta_prime.__all__
+from .delta_prime_solver import *
+__all__ = delta_prime_solver.__all__
