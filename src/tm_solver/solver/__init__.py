d491612d4dd1dd0a0745405975f4a9b070d5378c
Jérémy Salm <130314261+jeremslm@users.noreply.github.com>
2024-06-03 14:55:31 +0200

    Update __init__.py

--- src/tm_solver/solver/__init__.py
+++ src/tm_solver/solver/__init__.py
@@ -4,4 +4,4 @@ WRITTEN BY : Salm Jérémy, EPFL (jeremy.salm@epfl.ch)
 CREATED IN : April 2024
 """
 from .delta_prime_solver import *
-__all__ = delta_prime_solver.__all__
+#__all__ = delta_prime_solver.__all__ Creates an error if it's uncommented
